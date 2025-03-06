dde5266e9a64a182a847bd492abae79cb6ba7a5d
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-03-06 15:25:37 -0500

    fixing

--- aiffamp3.js
+++ aiffamp3.js
@@ -1,16 +1,22 @@
 // 123
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path'); // Add path module
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/public', express.static('public'));
+// app.use('/public', express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Use path.join for dynamic paths
 });
 
 
@@ -26,3 +32,6 @@ process.on('SIGTERM', gracefulShutdown);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
