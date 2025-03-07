b5c6b91bf50c307bc5e1f740a7db093200068e12
Zouhir Bensmaia <freelancebenz@gmail.com>
2025-03-07 11:51:36 -0500

    todos written

--- aiffamp3.js
+++ aiffamp3.js
@@ -1,7 +1,7 @@
 // 1231
 const express = require('express');
 const dotenv = require('dotenv');
-// const path = require('path'); // Add path module
+// const path = require('path');
 
 
 dotenv.config();
@@ -10,14 +10,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use('/public', express.static('public'));
-// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Use path.join for dynamic paths
-// });
 
 
 const gracefulShutdown = () => {
